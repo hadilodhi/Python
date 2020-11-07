1297d38f647c7d4e13b1f27b0f86db767ca793d8
Ahmad Lodhi <ecyal2@nottingham.edu.my>
2020-11-08 04:17:09 +0800

    Done

--- Leopards.py
+++ Leopards.py
@@ -6,9 +6,14 @@ import time
 import csv
 import os
 
-
 # Clear terminal
 os.system('cls')
+
+# Define Variables
+countries = []
+countries2 = []
+failed = []
+empty_lines = 0
 
 # Setting parameters for selenium to work
 options = webdriver.ChromeOptions()
@@ -17,12 +22,15 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://leopardscourier.com/pk/services/rate-calculator/')
 
 # Read CSV File
-leopardscsv = open('Leopards.csv',"r")
+leopardscsv = open('Leopards.csv', "r")
 reader = csv.reader(leopardscsv)
-countries = []
-failed = []
 for row in reader:
-    countries.append([row [0], row[1]])
+    if not ''.join(row).strip():
+        empty_lines += 1 
+        continue
+    countries.append([row [0], row[1], row[2]])
+leopardscsv.close()
+
 # Close Cookie Message
 driver.implicitly_wait(5)
 driver.find_element_by_xpath("/html/body/div[2]/span/a[2]").click()
@@ -44,14 +52,25 @@ for row in countries:
         driver.find_element_by_id("Weight").send_keys(row[1])
         driver.find_element_by_id("submit").click()
         time.sleep(3)
-        #Guess = driver.find_elements_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[3]/div[2]/div/div/span").text
-        #print("Price: ")
+        edit = driver.find_element_by_class_name("demo-container").text
+        edit = edit.replace("AMOUNT: ","")
+        edit = edit.replace(" PKR","")
+        edit = edit.split(".", 1)[0]
+        row[2] = edit.replace(",",".")
+        countries2.append([row [0], row[1], row[2]])
         driver.get('https://leopardscourier.com/pk/services/rate-calculator/')
     except:
         failed.append(countries)
         pass
 
+#Write CSV File
+leopardscsvs = open('Leopards.csv', "w", newline='')
+writer = csv.writer(leopardscsvs)
+for row in countries2:
+    writer.writerow([row [0], row[1], row[2]])
+leopardscsvs.close()
 
+#Done
 driver.quit()
 if len(failed) > 0:
         print("%s cases have failed" % len(failed))
