6cb0acaa35be358e57ab50d8ebd4c8fd09019019
Ahmad Lodhi <ecyal2@nottingham.edu.my>
2020-11-08 05:21:18 +0800

    Final

--- Leopards.py
+++ Leopards.py
@@ -8,6 +8,8 @@ import os
 
 # Clear terminal
 os.system('cls')
+print("Procedure starting")
+time.sleep(1)
 
 # Define Variables
 countries = []
@@ -38,41 +40,55 @@ driver.find_element_by_xpath("/html/body/div[2]/span/a[2]").click()
 # Commands
 for row in countries:
     try:
+        # Type
         driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/select/option[3]").click()
         driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[3]/div[2]/div/div/div[2]/span/span[1]/span/span[1]").click()
+        # Services
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys("INTERNATIONAL XPS")
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys(Keys.ENTER)
+        # Origin
         driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[3]/div[2]/div/div/div[3]/span/span[1]/span/span[1]").click()
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys("Karachi")
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys(Keys.ENTER)
+        # Destination
         driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[3]/div[2]/div/div/div[4]/div/span/span[1]/span/span[1]").click()
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys(row[0])
         driver.find_element_by_xpath("/html/body/span/span/span[1]/input").send_keys(Keys.ENTER)
+        # Weight
         driver.find_element_by_id("Weight").clear()
         driver.find_element_by_id("Weight").send_keys(row[1])
+        # Submit
         driver.find_element_by_id("submit").click()
-        time.sleep(3)
+        time.sleep(1)
+        # Formatting
         edit = driver.find_element_by_class_name("demo-container").text
         edit = edit.replace("AMOUNT: ","")
         edit = edit.replace(" PKR","")
         edit = edit.split(".", 1)[0]
         row[2] = edit.replace(",",".")
+        # Append
         countries2.append([row [0], row[1], row[2]])
+        # Reload
         driver.get('https://leopardscourier.com/pk/services/rate-calculator/')
     except:
         failed.append(countries)
         pass
 
-#Write CSV File
+# Write CSV File
 leopardscsvs = open('Leopards.csv', "w", newline='')
 writer = csv.writer(leopardscsvs)
 for row in countries2:
     writer.writerow([row [0], row[1], row[2]])
 leopardscsvs.close()
 
-#Done
+# Done
 driver.quit()
 if len(failed) > 0:
         print("%s cases have failed" % len(failed))
 else:
     print("Procedure concluded")
+    print("Opening file in excel")
+time.sleep(5)
+currentDirectory = os.getcwd()
+currentDirectory = currentDirectory + r"\Leopards.csv"
+os.startfile(currentDirectory)
